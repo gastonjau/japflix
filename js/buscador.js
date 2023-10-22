817c1b2329409730a508d0e893a75d252959317a
Gaston Jaurena <gassjau@gmail.com>
2023-10-22 11:06:12 -0300

    Ultima Act

--- js/buscador.js
+++ js/buscador.js
@@ -5,56 +5,109 @@ document.addEventListener("DOMContentLoaded", ()=>{
     .then(data =>{ btnBuscar.addEventListener("click", handleFunction )
         let buscar = document.getElementById("inputBuscar");
         let lista = document.getElementById("lista");
+
+
         function calificacion(score) {
-            if (score == "5") {
-              return `
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                `;
-            } else if (score == "4") {
-              return `
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="far fa-star"></i>
-                `;
-            } else if (score == "3") {
-              return `
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                `;
-            } else if (score == "2") {
-              return `
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                `;
-            } else if (score == "1") {
-              return `
-                  <i class="fas fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                `;
-            } else if (score == "0") {
-              return `
-                  Sin calificación.
-                `;
-            }
-            return "Puntuación no válida";
+          if (score == "8") {
+            return `
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>`
+              }
+          if (score == "7") {
+            return `
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star"></i>`
+              }
+          if (score == "6") {
+            return `
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star"></i>
+                <i class="bi bi-star"></i>`
+              }
+          if (score == "5") {
+            return `
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star"></i>
+                < i class="bi bi-star"></>
+                <i class="bi bi-star"></i>
+              `;
+          } else if (score == "4") {
+            return `
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+              `;
+          } else if (score == "3") {
+            return `
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+              `;
+          } else if (score == "2") {
+            return `
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+              `;
+          } else if (score == "1") {
+            return `
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+              `;
+          } else if (score == "0") {
+            return `
+                Sin calificación.
+              `;
           }
+          return "Puntuación no válida";
+        }
 
           
+
+
+
 
         function handleFunction (){
             lista.innerHTML = "";
@@ -68,6 +121,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
                                                 cosa.overview.toLowerCase().startsWith(searchValue) ?
                                                 lista.innerHTML +=
                                                 `<li class="list-group-item text-center color1">${cosa.title} <br />${cosa.tagline}
+                                                  <div class="amarillo ">
+                                                  <p>Calificacion: </p>${calificacion(Math.round(cosa.vote_average))}
+                                                  </div>
+                                                
                                                 </li>
                                                 ` : "" } )
             console.log(peliculasFiltradas)
